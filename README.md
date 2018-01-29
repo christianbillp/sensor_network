b9c6b8f162eb91f77384a64d71610c59d8041ac0
christianbillp <christianbillp@gmail.com>
2018-01-29 13:09:54 +0100

    Working version using mongo

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # sensor_network
+
+mongo.conf should have the following format:
+
+'''
+[conf]
+username=USERNAME
+password=PASSWORD
+hostname=HOSTNAME
+port=PORT (default is 27017)
+db=DATABASE
+'''
--- mongofront.py
+++ mongofront.py
@@ -2,6 +2,10 @@
 from pymongo import MongoClient
 import configparser
 import pandas as pd
+from datetime import datetime
+from bokeh.plotting import figure, output_file, save
+from bokeh.models import Range1d, LinearAxis
+from bokeh.layouts import gridplot
 
 class SensorFront():
 
@@ -17,28 +21,33 @@ class SensorFront():
 
         self.nodes = nodes
 
-    def get_all(self, node_id, n):
+    def get_all(self, node_id):
         """Get all values from database"""
         with MongoClient('mongodb://{}:{}@{}:{}/{}'.
                          format(self.username, self.password, self.hostname, self.port, self.db)) as client:
             values = [value for value in client['sensors'][node_id].find()]
+        df = pd.DataFrame(values)
+        df['timestamp'] = df['timestamp'].apply(datetime.fromtimestamp)
+        df['node'] = node_id
 
-        return values
+        return df
 
     def get_n_latest(self, node_id, n):
         """Gets [n] latest values from [node_id]"""
         with MongoClient('mongodb://{}:{}@{}:{}/{}'.
                          format(self.username, self.password, self.hostname, self.port, self.db)) as client:
-            values = [value for value in client['sensors'][node_id].find().sort('{$natural: -1}').limit(n)]
+            values = [value for value in client['sensors'][node_id].find().sort('timestamp', -1).limit(n)]
 
-        return values
+        df = pd.DataFrame(values)
+        df['timestamp'] = df['timestamp'].apply(datetime.fromtimestamp)
 
-    def combine(self, n):
+        return df
+
+    def get_dataframe(self, n):
         """Combines [n] entries from all nodes into a dataframe"""
         temp = []
-        for node in self.nodes:
+        for node in self.nodes.keys():
             df = pd.DataFrame(self.get_n_latest(node, n))
-            df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
             df['node'] = node
             temp.append(df)
 
@@ -46,23 +55,51 @@ class SensorFront():
 
         return df
 
+    def generate_html(self, n):
+        """Generates html for info page"""
+        pl = {}
+        for node in self.nodes.keys():
+            output_file("graphs.html")
+            df = sf.get_n_latest(node, n)
 
+            pl[node] = figure(x_axis_type='datetime', plot_width=800, plot_height=350,
+                       title="{} - {}".format(node, self.nodes[node]), toolbar_location="above",
+                       tools = "pan, wheel_zoom, box_zoom, reset",
+                       y_range=Range1d(16, 30))
+            pl[node].line(df['timestamp'], df['temperature'], color='blue')
+            pl[node].yaxis.axis_label = 'Degrees C'
+            pl[node].extra_y_ranges = {"humidity": Range1d(start=20, end=60)}
+            pl[node].add_layout(LinearAxis(y_range_name="humidity", axis_label='% humidity'), 'right')
+            pl[node].line(df['timestamp'], df['humidity'], y_range_name="humidity", color='green')
+
+        save(gridplot([[pl[node]] for node in self.nodes.keys()]))
+
+        with open('info.html', 'w') as f:
+            graphs = open('graphs.html', 'r').read()
+#            f.write("""<?php include 'protect.php';?>""")
+            f.write('<h1>Temperature and Humidity plots</h1>')
+            f.write(graphs)
 
 
 if __name__ == '__main__':
-    sf = SensorFront(['node0', 'node1', 'node2', 'node3'])
-#    ll=sf.get_latest('node0')
-#    ll=sf.get_ll('node0', 7)
+    # Construct SensorFront object from dictionary
+    sf = SensorFront({'node0' : 'bedroom', 'node1' : 'kitchen', 'node2' : 'living room', 'node3' : 'office', })
 
-    df = sf.combine(10)
+    # Get all values from node0
+    node0_all = sf.get_all('node0')
 
+    # Get 100 values from node0
+    latest100 = sf.get_n_latest('node1', 100)
 
+    # Get latest value from database
+    latest = sf.get_dataframe(1)
+    print(latest)
 
-#%%
-df.groupby('node')['humidity'].plot()
+    # Create a dataframe from the latest 500 values
+    df = sf.get_dataframe(1000)
 
+    # Create html page for visualization
+    sf.generate_html(3000)
 
-#%%
-
-#%%
+#%% End of File
 
